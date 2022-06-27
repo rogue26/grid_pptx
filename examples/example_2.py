20976abab0c649b9c030ef6602c2dadebe287ab3
max <max.hill@pm.me>
2022-06-26 20:35:06 -0600

    various changes

--- examples/example_2.py
+++ examples/example_2.py
@@ -5,6 +5,15 @@ import pandas as pd
 if __name__ == '__main__':
     df = pd.DataFrame({'a': [1, 2, 9], 'b': [4, 1, 6], 'c': [7, 8, 2]})
 
+    p = GridPresentation(
+        # template='greenblue.pptx',
+        slide_size='16_9',
+        header_height=1.5,
+        footer_height=1.0,
+        right_margin=0.25,
+        left_margin=0.25
+    )
+
     chart = AreaChart(df=df, stacked=True, normalized=False)
     table = Table(df)
     text = Text(text='some explanatory text here')
@@ -15,30 +24,7 @@ if __name__ == '__main__':
                         Row(6, table),
                         Row(6, text)))
 
-    p = GridPresentation(
-        # template='greenblue.pptx',
-        slide_size='16_9',
-        header_height=1.5,
-        footer_height=1.0,
-        right_margin=0.25,
-        left_margin=0.25
-    )
-
-    for test_param in range(5):
-        # do some analysis
-        # create a dataframe
-
-        chart = AreaChart(df=df, stacked=True, normalized=False)
-        table = Table(df)
-        text = Text(text='some explanatory text here')
-
-        design = Row(12,
-                     Column(6, chart),
-                     Column(6,
-                            Row(6, table),
-                            Row(6, text)))
-
-        p.add_slide(layout_num=5, design=design, title='my shiny analysis')
+    p.add_slide(layout_num=5, design=design, title='my shiny analysis')
 
     filename = 'example_2.pptx'
 
